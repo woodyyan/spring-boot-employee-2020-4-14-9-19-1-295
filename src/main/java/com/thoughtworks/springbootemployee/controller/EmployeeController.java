f7d735071f089b509ee0abf19a5c75454333d3b3
Marco Lam <lamma3@oocl.com>
2020-04-15 17:29:14 +0800

    add get all employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,9 +1,23 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return employeeService.getAll();
+    }
 }
---
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAll() {
+        return employeeRepository.findAllEmployee();
+    }
+}
